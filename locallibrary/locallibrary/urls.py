e62c418ba30f030c3c6e19222483846b446985c2
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-10-22 17:06:58 -0700

    Adds catalog app, sets up url mapper, and adds models for LocalLibrary

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -14,8 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Use include() to add paths from the catalog application
+    path('catalog/', include('catalog.urls')),
+
+    # Add URL maps to redirect the base URL to the application
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
+
+# Use static() to add url mapping to serve static files during development (only)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
