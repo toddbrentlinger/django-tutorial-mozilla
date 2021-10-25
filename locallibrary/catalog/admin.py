6f9e83ce6d88a3d871685249a35a86bf9f076b19
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-10-24 18:04:56 -0700

    Registers models, adds to database through admin panel, and starts to improve admin interface

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Author, Genre, Language, Book, BookInstance
 
 # Register your models here.
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
+#admin.site.register(BookInstance)
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for the Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+# Register the Admin classes for the BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
