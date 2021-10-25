7c0680db8c7da1924d21b8b62436def59dae9fd8
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-10-25 15:31:43 -0700

    Finishes admin site improvements and creates home page by extending base template.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+def index(request):
+    """View function  for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default
+    num_authors = Author.objects.count()
+
+    # Books that contain word 'the' (case insensitive)
+    num_books_icontains_word = Book.objects.filter(title__icontains='the').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_genres': num_genres,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_books_icontains_word': num_books_icontains_word,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
