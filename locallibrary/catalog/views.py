b70a63d4fbf02af3cf55f035d4858eb623092e27
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-10-28 16:08:43 -0700

    Uses session framework to add number of visits to site, creates groups with test users, and adds login and logout templates for authentication views.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -20,6 +20,10 @@ def index(request):
     # Books that contain word 'the' (case insensitive)
     num_books_icontains_word = Book.objects.filter(title__icontains='the').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -27,6 +31,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_books_icontains_word': num_books_icontains_word,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -27,6 +27,9 @@ urlpatterns = [
 
     # Add URL maps to redirect the base URL to the application
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
+
+    # Add Django site authentication urls (for login, logout, password management)
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
